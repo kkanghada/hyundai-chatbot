27e926b9dd800ae9edf5ef4054ed69ba36eeafa8
kkanghada <kkanghada@gmail.com>
2025-03-13 18:00:05 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -90,4 +90,6 @@ def message():
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 3001))
+    app.run(host="0.0.0.0", port=port, debug=True)
